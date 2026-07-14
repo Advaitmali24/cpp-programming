9aedd1742fcb96ac358c4e9b442af0af6caeb7c3
Advaitmali24 <maliadvait07@gmail.com>
2026-07-14 15:49:22 +0530

    Add files via upload

---
+++ mod.c
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"enter first number:";
+cin>>a;
+
+cout<<"enter second number:";
+cin>>b;
+
+cout<<"Addition ="<<a+b<<endl;
+cout<<"Substraction ="<<a-b<<endl;
+cout<<"Multiplication ="<<a*b<<endl;
+cout<<"Division ="<<a/b<<endl;
+cout<<"Modulus ="<<a%b<<endl;
+
+return 0;
+}
---
+++ swap.c
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+
+cout<<"enter first number:";
+cin>>a;
+
+cout<<"enter second number:";
+cin>>b;
+
+cout<<"enter number after swapping:";
+cin>>c;
+c=a;
+a=b;
+b=c;
+cout<<"after swapping:"<<endl;
+
+
+return 0;
+}
+
